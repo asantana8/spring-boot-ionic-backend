a27e1f85ee770563cdcd154a1fe65b90be115d85
Adriano Santana <asantana8@gmail.com>
2018-03-06 15:40:02 -0300

    Inclusão das classe Cliente, Endereco, Cidade e TipoCliente

---
+++ src/main/java/com/asantana/cursomc/services/CidadeService.java
@@ -0,0 +1,24 @@
+package com.asantana.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.asantana.cursomc.domain.Cidade;
+import com.asantana.cursomc.repositories.CidadeRepository;
+import com.asantana.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class CidadeService {
+	
+	@Autowired
+	private CidadeRepository repo;
+	
+	public Cidade buscar(Integer id) {
+		Cidade obj = repo.findOne(id);
+		if(obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Cidade.class.getName()); 
+		}
+		return obj;
+	}
+
+}
