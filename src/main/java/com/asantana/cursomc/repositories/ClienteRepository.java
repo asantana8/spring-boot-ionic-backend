ed0db842e7783cb807b4129a5eddf75d82fb3bb8
t_acsantana <t_acsantana@trf5.jus.br>
2018-04-24 09:46:26 -0300

    Validacao de email no insert

--- src/main/java/com/asantana/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/asantana/cursomc/repositories/ClienteRepository.java
@@ -1,5 +1,7 @@
 package com.asantana.cursomc.repositories;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,6 @@ import com.asantana.cursomc.domain.Cliente;
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
 
-	
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 }
